ad3e1c9d0bc22e6ec76d3d2faea24a650d360bda
Alexandre Knihar <77019887+bumpybanana@users.noreply.github.com>
2021-05-01 16:35:32 +0200

    Update train.py

--- train.py
+++ train.py
@@ -7,29 +7,35 @@ from dataset import DepthDataset
 from utils import (
     load_checkpoint,
     save_checkpoint,
-    get_loaders,
     check_accuracy,
     save_predictions_as_imgs,
 )
 from NN7conv import DepthPredictionNet
 
+
 #Hyperparameters
-
-
 lr = 0.01
+#device = "cpu"
 device = "cuda" if torch.cuda.is_available() else "cpu"
 batch_size = 5
-num_epochs = 100
+num_epochs = 1
 num_workers = 0 # ~batch_size
 img_height = 320
 img_width = 320
 pin_memory = True
 load_model = False
+transformation = transforms.RandomCrop(320)
 
 train_rgb_folder =  "E:/nyuv2/train_rgb/train_rgb"
 train_depth_folder = "E:/nyuv2/train_depth"
 test_rgb_folder = "E:/nyuv2/test_rgb"
 test_depth_folder = "E:/nyuv2/test_depth"
+
+train_dataset = DepthDataset(train_rgb_folder,train_depth_folder,transformation)
+test_dataset = DepthDataset(test_rgb_folder,test_depth_folder,transformation)
+
+trainloader = torch.utils.data.DataLoader(train_dataset,batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=pin_memory)
+testloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=pin_memory)
 
 def train_fn(loader,model,optimizer,loss_fn,scaler):
     loop = tqdm(loader)
@@ -57,34 +63,19 @@ def train_fn(loader,model,optimizer,loss_fn,scaler):
 
 
 def main():
-    transformation = transforms.Compose([transforms.RandomCrop(320),
-                                         transforms.RandomHorizontalFlip(p=0.5),
-                                         transforms.RandomVerticalFlip(p=0.5),
-                                         transforms.RandomAffine(4.5,scale=(1, 1.5)),
-                                       # transforms.ColorJitter()
-                                         transforms.ToTensor()])
 
     model = DepthPredictionNet().to(device)
     loss_fn = nn.L1Loss()
     optimizer = optim.SGD(model.parameters(),lr=lr,momentum=0.9,weight_decay=1e-4)
 
-    train_loader, test_loader = get_loaders(
-        train_rgb_folder,
-        train_depth_folder,
-        test_rgb_folder,
-        test_depth_folder,
-        batch_size,
-        transformation,
-        num_workers,
-        pin_memory
-    )
+
     if load_model:
         load_checkpoint(torch.load("my_checkpoint.pth.tar"),model)
-    #check_accuracy(test_loader,model,device=device)
+    check_accuracy(testloader,model,device=device)
     scaler = torch.cuda.amp.GradScaler()
 
     for epoch in range(num_epochs):
-        train_fn(train_loader,model,optimizer,loss_fn,scaler)
+        train_fn(trainloader,model,optimizer,loss_fn,scaler)
 
         #save model
         checkpoint = {
@@ -94,13 +85,16 @@ def main():
         save_checkpoint(checkpoint)
 
         #check accuracy
-        #check_accuracy(test_loader,model,device=device)
+        check_accuracy(testloader,model,device=device)
 
         #print examples to a folder
         save_predictions_as_imgs(
-            test_loader,model,folder="saved_images/",device=device
+            testloader,model,folder="saved_images/",device=device
         )
 
+
+if __name__ == "__main__":
+    main()
 
 if __name__ == "__main__":
     main()
