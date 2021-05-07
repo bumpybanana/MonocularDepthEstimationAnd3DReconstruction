091c99bf5f541b3b85ac703f16e1868ccea07b88
Alexandre Knihar <77019887+bumpybanana@users.noreply.github.com>
2021-05-07 20:36:31 +0200

    Update trainsqueeze.py

--- trainsqueeze.py
+++ trainsqueeze.py
@@ -11,18 +11,18 @@ from trainutils import (
     save_predictions_as_imgs_squeeze,
 )
 from squeezeunet import Model
-
+import matplotlib.pyplot as plt
 
 #Hyperparameters
 lr = 0.01
-#device = "cpu"
-device = "cuda" if torch.cuda.is_available() else "cpu"
+device = "cpu"
+#device = "cuda" if torch.cuda.is_available() else "cpu"
 batch_size = 5
 num_epochs = 150
 num_workers = 0 # ~batch_size
 pin_memory = True
 load_model = False
-transformation = transforms.RandomCrop(320)
+#transformation = transforms.RandomCrop(320)
 
 #train_rgb_folder = "C:/Users/alexk/PycharmProjects/test/data/train_rgb/"
 #train_depth_folder = "C:/Users/alexk/PycharmProjects/test/data/train_depth"
@@ -41,50 +41,31 @@ test_dataset = DepthDataset(test_rgb_folder, test_depth_folder)
 trainloader = torch.utils.data.DataLoader(train_dataset,batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=pin_memory)
 testloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=pin_memory)
 
-def train_fn(loader,model,optimizer,loss_fn,scaler):
-    loop = tqdm(loader)
-
-    for batch_index, (data, targets) in enumerate(loop):
-        data = data.to(device=device)
-        #targets = targets.float().to(device=device)
-        targets = targets.to(device=device)
-
-        #forward
-        predictions = model(data)
-        loss = loss_fn(predictions, targets)
-
-
-        #backward
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        total_loss += loss.item()
-        scaler.scale(loss).backward()
-        scaler.step(optimizer)
-        scaler.update()
-
-        #update tqdm loop
-        loop.set_postfix(loss=loss.item())
-
+def init_weights(m):
+    if type(m) == nn.Conv2d:
+        torch.nn.init.xavier_uniform_(m.weight)
+    if type(m) == nn.ConvTranspose2d:
+        torch.nn.init.xavier_uniform_(m.weight)
+    #if type(m) == nn.BatchNorm2d:
+    #   torch.nn.init.xavier_uniform_(m.weight)
 
 def main():
     #print("cuda" if torch.cuda.is_available() else "cpu")
     model = Model().to(device)
+    model.apply(init_weights)
     loss_fn = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=1e-4)
-
+    losses = []
 
     if load_model:
         load_checkpoint(torch.load("my_checkpoint.pth.tar"), model)
 
-    #scaler = torch.cuda.amp.GradScaler()
 
     for epoch in range(num_epochs):
        #train_fn(trainloader, model, optimizer, loss_fn)
         loop = tqdm(trainloader)
-        total_loss = 0
-
+        running_loss = 0
+        epoch = 0
         for batch_index, (data, targets) in enumerate(loop):
             data = data.to(device=device)
             # targets = targets.float().to(device=device)
@@ -99,10 +80,21 @@ def main():
             loss.backward() #calculate gradients
             optimizer.step() #update weights
 
-            total_loss += loss.item()
+            running_loss += loss.item() * data.size(0)
 
             # update tqdm loop
             loop.set_postfix(loss=loss.item())
+
+        epoch_loss = running_loss /len(trainloader)
+        losses.append(epoch_loss)
+        epoch = epoch +1
+        print('Epoch: ', epoch)
+        print('Total loss: ',running_loss)
+        print('Loss per epoch: ',epoch_loss)
+
+        plt.plot(losses)
+
+
 
         #save model
         checkpoint = {
@@ -116,7 +108,7 @@ def main():
 
         #print examples to a folder
         save_predictions_as_imgs_squeeze(
-            testloader, model, folder="saved_images7x7/", device=device
+            testloader, model, folder="saved_images_squeeze/", device=device
         )
 
 
@@ -125,4 +117,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
