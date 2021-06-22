b2f5ea8c896fbad85a1fe8c65a4f73dd7a0c6ba6
Alexandre Knihar <77019887+bumpybanana@users.noreply.github.com>
2021-06-22 20:38:27 +0200

    Create dataset.py

---
+++ depthestimation/dataset.py
@@ -0,0 +1,37 @@
+import os
+import torch
+from PIL import Image
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+import numpy as np
+
+
+class DepthDataset(Dataset):
+    def __init__(self,rgb_dir,depth_dir,transform=None, transform_rgb=None):
+        self.rgb_dir = rgb_dir
+        self.depth_dir = depth_dir
+        self.transform = transform
+        self.transform_rgb = transform_rgb
+        self.images = os.listdir(rgb_dir)
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, index):
+        rgb_path = os.path.join(self.rgb_dir, self.images[index])
+        depth_path = os.path.join(self.depth_dir, self.images[index])
+        rgb = transforms.ToTensor()(np.array(Image.open(rgb_path).convert("RGB")))
+        depth = transforms.ToTensor()(np.array(Image.open(depth_path))) / (2**16)
+
+        concat = torch.cat([rgb, depth])
+        concat = transforms.ToPILImage()(concat)
+
+        if self.transform is not None:
+            concat = self.transform(concat)
+            concat = torch.split(concat,3,0)
+            rgb = concat[0]
+            depth = concat[1]
+        if self.transform_rgb is not None:
+            rgb = self.transform_rgb(rgb)
+
+        return rgb, depth
